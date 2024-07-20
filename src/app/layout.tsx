1e1002620b29650a3788f7121716ea7815afaab8
Anurag Dhote <anuragdhote392@gmail.com>
2024-07-21 03:22:53 +0530

    chrore - providers  cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,34 @@
 import "~/styles/globals.css";
 
-// import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { Inter } from "next/font/google"
 
-import { TRPCReactProvider } from "~/trpc/react";
-import { HydrateClient } from "~/trpc/server";
-
-import { AuthStoreProvider } from "~/providers/auth-store-provider";
-
+import Providers from "~/providers";  
 import { Navbar } from "./_components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ecommerce Project",
-  description: "Complete Ecommerce Store. Select from 100+ Categories!",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+	title: "Ecommerce Project",
+	description: "Complete Ecommerce Store. Select from 100+ Categories!",
+	icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={inter.className}>
-      <body>
-        <TRPCReactProvider>
-          <HydrateClient>
-            <AuthStoreProvider>
-              <header>
-                <Navbar />
-              </header>
-              <main>
-                {children}
-              </main>
-            </AuthStoreProvider>
-          </HydrateClient>
-        </TRPCReactProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={inter.className}>
+			<body>
+				<Providers>
+					<header>
+						<Navbar />
+					</header>
+					<main>
+						{children}
+					</main>
+				</Providers>
+			</body>
+		</html>
+	);
 }
---
+++ src/providers/index.tsx
@@ -0,0 +1,19 @@
+
+
+import { ReactNode } from 'react'
+
+import { AuthStoreProvider } from "~/providers/auth-store-provider";
+import { TRPCReactProvider } from "~/trpc/react";
+import { HydrateClient } from "~/trpc/server";
+
+export default function Providers({ children }: { children: ReactNode }) {
+    return (
+        <TRPCReactProvider>
+            <HydrateClient>
+                <AuthStoreProvider>
+                    {children}
+                </AuthStoreProvider>
+            </HydrateClient>
+        </TRPCReactProvider>
+    )
+}
